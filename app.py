625499f74497e70145a73ffdba220c1bbb670c0a
Lameuhton <aurelianeg2000@gmail.com>
2025-11-16 17:35:48 +0100

    Add Dockerfile and initial app.py implementation

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 import requests
+
+def main():
+    url = "https://v2.jokeapi.dev/joke/Any"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data.get("type") == "single":
+        print("Blague: ",data.get("joke"))
+    else:
+        print("Blague: ",data.get("setup"))
+        print("Réponse: ",data.get("delivery"))
+    
+
+if __name__ == "__main__":
+    main()
